cac8d30193b8c2ab8383018bab5e73338a01baab
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-11-26 15:07:46 +0800

    Update ft_print.c

--- ft_print.c
+++ ft_print.c
@@ -1,8 +1,24 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdarg.h>
+#include "printf.h"
 
-int ft_printf(const char *, ...)
+int     ft_printf(const char *format, ...)
 {
-  
+        va_list args;
+        int             count;
+
+        count = 0;
+        va_start(args, format);
+        while (*format)
+        {
+                if (*format == '%')
+                {
+                }
+                else
+                {
+                        write(1, format, 1);
+                        count ++;
+                }
+                format ++;
+        }
+        va_end(args);
+        return (count);
 }
