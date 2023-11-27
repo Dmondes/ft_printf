aab71f49a8a604563da881403e8c2447e037b0c1
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-11-27 17:02:00 +0800

    Update ft_print.c

--- ft_print.c
+++ ft_print.c
@@ -22,3 +22,23 @@ int     ft_printf(const char *format, ...)
         va_end(args);
         return (count);
 }
+
+int     find_format(const char  *format, int    i)
+{
+        if (format[i] == 'c')
+                ft_pchar();
+        else if (format[i] == 's')
+                ft_pstr();
+        else if (format[i] == 'd' || format[i] == 'i')
+                ft_pint();
+        else if (format[i] == 'u')
+                ft_punsign();
+        else if (format[i] == 'p')
+                ft_ppoint();
+        else if (format[i] == 'x')
+                ft_plower();
+        else if (format[i] == 'X')
+                ft_pupper();
+        else if (format[i] =='%')
+                ft_ppercent();
+}
