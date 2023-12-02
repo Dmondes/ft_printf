79ad8de46c73d7c2f0cc9613557b15efcba0e080
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-12-02 17:19:10 +0800

    Update ft_print.c

--- ft_print.c
+++ ft_print.c
@@ -1,45 +1,53 @@
-#include "printf.h"
+#include "ft_printf.h"
 
-int     ft_printf(const char *format, ...)
+int	find_format(char c, va_list args);
+
+int	ft_printf(const char *format, ...)
 {
-        va_list args;
-        int             count;
+	va_list	args;
+	int		count;
 
-        count = 0;
-        va_start(args, format);
-        while (*format)
-        {
-                if (*format == '%')
-                {
-                    find_format(*format, args);
-                }
-                else
-                {
-                        write(1, format, 1);
-                        count ++;
-                }
-                format ++;
-        }
-        va_end(args);
-        return (count);
+	if (format == NULL)
+		return (-1);
+	count = 0;
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format ++;
+			count += find_format(*format, args);
+		}
+		else
+		{
+			write(1, format, 1);
+			count ++;
+		}
+		format ++;
+	}
+	va_end(args);
+	return (count);
 }
 
-int     find_format(char c, va_list args)
+int	find_format(char c, va_list args)
 {
-        if (c == 'c')
-                ft_pchar(args);
-        else if (c == 's')
-                ft_pstr(args);
-        else if (c == 'd' || c == 'i')
-                ft_pint(args);
-        else if (c == 'u')
-                ft_punsign(args);
-        else if (c == 'p')
-                ft_ppoint(args);
-        else if (c == 'x')
-                ft_plower(args);
-        else if (c == 'X')
-                ft_pupper(args);
-        else if (c =='%')
-                ft_ppercent(args);
-}
+	int	count;
+
+	count = 0;
+	if (c == 'c')
+		count += ft_pchar(va_arg(args, int));
+	else if (c == 's')
+		count += ft_pstr(va_arg(args, char *));
+	else if (c == 'd' || c == 'i')
+		count += ft_pint(va_arg(args, int));
+	else if (c == 'u')
+		count += ft_punsign(va_arg(args, unsigned int));
+	else if (c == 'p')
+		count += ft_ppoint(va_arg(args, unsigned long int));
+	else if (c == 'x')
+		count += ft_plower(va_arg(args, unsigned int));
+	else if (c == 'X')
+		count += ft_pupper(va_arg(args, unsigned int));
+	else if (c == '%')
+		count += ft_ppercent();
+	return (count);
