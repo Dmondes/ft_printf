1e02c55e9d0dd763ee7c7d9c1ed75d27793a00f5
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-12-02 17:18:00 +0800

    Update printf.h

--- printf.h
+++ printf.h
@@ -1,9 +1,17 @@
 #ifndef PRINTF_H
-#define PRINTF_H
+# define PRINTF_H
 
-#include <stdarg.h>
-#include <unistd.h>
+# include <stdarg.h>
+# include <unistd.h>
 
-int     ft_printf(const char *, ...);
+int		ft_printf(const	char *format, ...);
+int		ft_pchar(int c);
+int		ft_pstr(char *str);
+int		ft_pint(int n);
+int		ft_punsign(unsigned int n);
+int		ft_ppoint(unsigned long	int ptr);
+int		ft_plower(unsigned int n);
+int		ft_pupper(unsigned int n);
+int		ft_ppercent(void);
 
 #endif
